156b3f533f6371975c4ef690f13bc43229b622ff
BT_C09_21 <mobileappdev@bths.edu>
2026-06-01 14:09:17 -0400

    Initial Commit

---
+++ Rasket Bandom/Rasket_BandomApp.swift
@@ -0,0 +1,32 @@
+//
+//  Rasket_BandomApp.swift
+//  Rasket Bandom
+//
+//  Created by Student on 6/1/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Rasket_BandomApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
